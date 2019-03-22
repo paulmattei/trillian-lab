7790bfd10fcd9da45c16bc3d8209890980645041
Paul Mattei <paulmattei@paulmattei-macbookpro.roam.corp.google.com>
2019-03-22 16:08:23 +0000

    refreshed configs from trillian github repo

--- kubernetes/example-config.sh
+++ kubernetes/example-config.sh
@@ -2,7 +2,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Set the following variables to the correct values for your Google Cloud
 # project and Kubernetes cluster.
-export PROJECT_ID="trillian-lab-project"
+export PROJECT_ID="GOOGLE CLOUD PROJECT ID HERE (https://cloud.google.com/resource-manager/docs/creating-managing-projects#identifying_projects)"
 export CLUSTER_NAME=trillian
 export REGION=us-east4
 export MASTER_ZONE="${REGION}-a"
